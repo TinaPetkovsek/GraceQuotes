ab982bd121d568988a6aca3739f653136c86d6e3
tinapetkovsek <tina1.petkovsek@gmail.com>
2024-03-06 17:57:38 +0100

    dodala prave tag-e

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Badge } from "@/components/ui/badge";
 export default function App() {
   const [quotes, setQuotes] = useState([]);
   const [tags, setTags] = useState(["tag1", "tag2", "tag3", "tag4"]);
+  const [selected_tag, setSelected_tag] = useState(null);
 
   async function getQuotes() {
     const request = await fetch("/quotes.json");
@@ -19,20 +20,47 @@ export default function App() {
   }, []);
 
   useEffect(() => {
-    console.log(quotes);
+    //ali quotes obstaja in ima elemente
+    if (!(quotes && quotes.length > 0)) {
+      //ce ne, prenehamo izvajati funkcijo
+      return;
+    }
+    //ce pridemo do sem, vemo da se zgornji if ni izvedel
+
+    let tags = []; //const nam preprečuje, da dodamo novo vrednost, namesto tega pišemo let
+
+    quotes.forEach((e) => {
+      //za vsak e -quote v quotes
+      //console.log(e["tags"]);
+      //tags = tags.concat(e["tags"]); izpiše vse tage - nekateri se ponavljajo, zato:
+      const quote_tags = e["tags"];
+      quote_tags.forEach((el) => {
+        //console.log(el);
+        if (tags.includes(el) != true) {
+          tags.push(el);
+        }
+      });
+
+      // 1. for loop za vse tage od tega quota (e["tags"])
+      // 2. za vsakega preverimo, ce je v seznamu
+      //      tags.include("FamousQuotes")
+      // 3. ce tega elementa ni, ga dodamo
+      //      tags.push("FamousQuotes")
+    });
+    setTags(tags);
   }, [quotes]);
 
   return (
     <div className="p-4">
       <div>
         {tags.map((tag) => (
-          <Badge>{tag}</Badge>
+          <Badge>{tag}</Badge> //dodaj onClick
         ))}
       </div>
 
       <div className="grid grid-cols-3 gap-4">
         {quotes.map((quote) => (
-          <Quote quote={quote}></Quote>
+          <Quote quote={quote} key={quote["_id"]}></Quote>
         ))}
       </div>
     </div>
